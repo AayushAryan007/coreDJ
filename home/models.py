afe17e6a26cae93aef62599b44ece1183346c85e
Aayush <unitedestates.ayush10@gmail.com>
2025-11-26 20:33:10 +0530

    migrations added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = models.AutoField() 
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True)
+    # image = models.ImageField()
+    # file = models.FileField()
+
+    def __str__(self):
+        return self.name
+#     student = Student.objects.create(name="Alice", age=30)
+
+# # Without __str__:
+# print(student)  # Output: <Student: Student object (1)>
+
+# # With __str__:
+# print(student)  # Output: Alice
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    
